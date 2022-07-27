71304077bb6c459849183b32b4205bfaf0cd748f
LorisP96 <lorispalazzo96@gmail.com>
2022-07-27 14:26:01 +0200

    funzione

--- index.php
+++ index.php
@@ -13,6 +13,10 @@ class Movie {
         $this->length = $_length;
         $this->image = $_image;
     }
+
+    function poster() {
+        echo 'poster';
+    }
 } 
 
 $inception = new Movie('Inception', 'Christopher Nolan', '2010', '2h 28m', "https://m.media-amazon.com/images/I/91Rc8cAmnAL._AC_SY679_.jpg");
@@ -31,13 +35,13 @@ $network = new Movie('The Social Network', 'David Fincher', '2010', '2h', "https
 <body>
     <div class="container">
         <div class="film">
-            <img src="<?php echo $inception->image ?>" alt="<?php echo $inception->title ?> poster">
+            <img src="<?php echo $inception->image ?>" alt="<?php echo $inception->title ?> <?php $inception->poster() ?>">
             <h2><?php echo $inception->title ?></h2>
             <div><?php echo $inception->year ?></h2> , <?php echo $inception->length ?></h2></div>
             <h4><?php echo $inception->director ?></h2></h4>
         </div>
         <div class="film">
-            <img src="<?php echo $network->image ?>" alt="<?php echo $network->title ?> poster">
+            <img src="<?php echo $network->image ?>" alt="<?php echo $network->title ?> <?php $network->poster() ?>">
             <h2><?php echo $network->title ?></h2>
             <div><?php echo $network->year ?></h2> , <?php echo $network->length ?></h2></div>
             <h4><?php echo $network->director ?></h2></h4>
